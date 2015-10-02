a5be3c40db5b35f9f2dc713bbc5c284c6d5b76f0
Dung Nguyen <dungntnew@gmail.com>
2015-10-02 12:05:42 +0900

    implement fserve api

--- fserve/app.js
+++ fserve/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: true
+})); // support encoded bodies
+
 var fs = require('fs');
 var path = require('path');
 var printer = require('./printer');
+var mysql = require('mysql');
+
 
 
 var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf8'));
@@ -10,17 +19,17 @@ var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config.json'), 
 printer.setup(config);
 
 
-app.get('/', function (req, res) {
-  res.send('api alive!');
+app.get('/', function(req, res) {
+	res.send('api alive!');
 });
 
-app.get('/info', function (req, res) {
-  res.json(config);
+app.get('/info', function(req, res) {
+	res.json(config);
 });
 
 var handler_error = function(res, message, error, variable) {
 	var message = ''
-	var reply =  {
+	var reply = {
 		status: false,
 		message: message,
 		error: error,
@@ -29,16 +38,85 @@ var handler_error = function(res, message, error, variable) {
 	res.json(reply);
 }
 
-app.get('/json', function(req, res){
+var fetch_order_info = function(order_id, callback) {
+	var connection = mysql.createConnection({
+		host: config.db_host,
+		port: config.db_port,
+		user: config.db_user,
+		password: config.db_password,
+		database: config.db_name
+	});
+	connection.connect();
 
-	var inpath = path.resolve(__dirname, 'json_data.json');
-	var outpath = path.resolve(__dirname, 'outfile.png');
-	
-	var printerHandler = function(success, err) {
+	var query = 'SELECT plg_kedit_json_path json_path';
+	query += ' SELECT plg_kedit_tpl_path template_path';
+	query += ' FROM dtb_order';
+	query += ' WHERE order_id=' + order_id;
+	query += ' LIMIT 1';
+
+	connection.query(query, function(err, rows, fields) {
+		if (err) {
+			callback(null, err);
+			return;
+		}
+
+		if (rows.length == 0) {
+			callback(null, 'not found order for order_id' + order_id);
+			return;
+		}
+		callback(rows[0]);
+	});
+	connection.end();
+}
+
+app.get('/api/export/:order_id', function(req, res) {
+
+
+	var order_id = req.params['order_id'];
+	fetch_order_info(order_id, function(order, err) {
+		if (err) {
+			return handler_error(res,
+				config.ERR_LOAD_ORDER,
+				err
+			);
+		}
+		exprtHandler(order);
+	});
+
+	var exprtHandler = function(order) {
+
+		var json_path = order.json_path;
+		fs.readFile(json_path, 'utf8', function(err, json) {
+
+			// handle load json fail error.
+			if (err) {
+				return handler_error(res,
+					config.ERR_LOAD_JSON,
+					err,
+					json_path
+				);
+			}
+
+			// build params for printer
+		    var export_dir = path.dirname(json_path);
+		    var export_name = path.basename(json_path, '.json');
+		    var export_path = path.resolve(export_dir, export_name + '.png');
+			var params = {
+				json: json,
+				template_path: order.template_path,
+				outpath: export_path
+			};
+
+			// start printer module
+			printer.handler(params, printerHandler);
+		});
+	}
+
+	var printerHandler = function(export_path, err) {
 		// handle printer error.
-		if (!success || err) {
-			return handler_error(res, 
-				config.ERR_EXPORT_PNG, 
+		if (!export_path || err) {
+			return handler_error(res,
+				config.ERR_EXPORT_PNG,
 				err
 			);
 		}
@@ -47,40 +125,15 @@ app.get('/json', function(req, res){
 			status: true,
 			message: 'export finish png',
 			error: null,
-			data: outpath 
+			data: export_path
 		});
 	};
-
-	fs.readFile(inpath, 'utf8', function(err, json){
-		
-		// handle load json fail error.
-		if (err) {
-			return handler_error(res, 
-				config.ERR_LOAD_JSON, 
-				err,
-				inpath
-			);
-		}
-        
-        // build params for printer
-		var params = {
-			json: json,
-			template_path: 'http://fabricjs.com/assets/logo.png',
-			outpath: outpath
-		};
-
-		// start printer module
-		printer.handler(params, printerHandler);
-	});
-
 });
 
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
