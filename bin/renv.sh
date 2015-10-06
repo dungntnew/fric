39d062a1c80492bb652dfbadeceaf4b1174d710c
Dung Nguyen <dungntnew@gmail.com>
2015-10-06 11:50:09 +0900

    add bin

---
+++ bin/renv.sh
@@ -0,0 +1,8 @@
+USER=eccube
+HOST=157.7.121.167
+PASSWORD=eccube
+
+ECC_ROOT=/var/www/html/eccube-2_13/
+HTML_PLG_DIR=/var/www/html/eccube-2_13/html/plugin/
+DATA_PLG_DIR=/var/www/html/eccube-2_13/data/downloads/plugin/
+TPL_CACHE_DIR=/var/www/html/eccube-2_13/data/Smarty/templates_c
---
+++ bin/sql.sh
@@ -0,0 +1,1 @@
+mysql -u eccube_db_user  eccube_db -pabcd1234 -e 'select * from dtb_customer limit 10 \G'; 
