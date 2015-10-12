1ab173b9a4a83a9e9505a4b8101eb092b5c02021
Dung Nguyen <dungntnew@gmail.com>
2015-10-12 14:35:16 +0900

    remove plugin config page

--- KEditPlugin/config.php
+++ KEditPlugin/config.php
@@ -1,12 +1,2 @@
 <?php
-
-// {{{ requires
-require_once PLUGIN_UPLOAD_REALDIR .  'KEditPlugin/LC_Page_Plugin_KEditPlugin_Config.php';
-
-// }}}
-// {{{ generate page
-$objPage = new LC_Page_Plugin_KEditPlugin_Config();
-register_shutdown_function(array($objPage, 'destroy'));
-$objPage->init();
-$objPage->process();
 ?>
